092dd488fedf5e7ff441670ccd72e9cd914342ec
Sivagiri S <101998903+SivagiriSSK@users.noreply.github.com>
2023-12-26 12:35:14 +0530

    Youtube Clone Using HTML & CSS

---
+++ youtube-v1/assets/scripts/main.js
@@ -0,0 +1,20 @@
+function handleClick() {
+  const list = document.getElementById("demoJavascript").classList;
+  list.add("myStyle");
+}
+
+function changeText() {
+  document.getElementById("changeContent").innerText = "Welcome to JavaScript!";
+}
+
+// add list using javascript
+
+function addMenu() {
+  let x = document.getElementById("userInput").value;
+  let li = document.createElement("li");
+}
+
+let userskills = document.getElementById("userSkill");
+function addUserSkills() {
+  document.getElementById("Skills").innerHTML = userskills;
+}
